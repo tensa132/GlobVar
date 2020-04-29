e2d1579e816d851a520cb26d4553d56aa54d6255
tensa132 <thundermist84@gmail.com>
2020-04-29 09:16:00 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GlobSerService } from './glob-ser.service';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  data:String;
+  constructor(public varglob : GlobSerService) {
+    this.data = this.varglob.getData();
+  }
 }
--- src/app/glob-ser.service.ts
+++ src/app/glob-ser.service.ts
@@ -5,5 +5,12 @@ export class GlobSerService {
 
   constructor() { }
 
-  private Data : String = "Test Global"
+  private Data : String = "Test Global";
+
+  getData() {
+    return this.Data;
+  }
+  setData(databaru) {
+    this.Data = databaru;
+  }
 }
